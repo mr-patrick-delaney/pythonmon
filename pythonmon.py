7d42bc3b62f2aecf3746ae24cfb9e197d5be2723
Patrick Delaney <viapurifico@gmail.com>
2025-03-31 22:57:36 +1100

    Create pythonmon.py, create Pythonmon class and _init_ method

---
+++ pythonmon.py
@@ -0,0 +1,12 @@
+class Pythonmon():
+    
+    def __init__(self, name, type, hp, defense, atk_name, atk_dmg):
+        self._name = name
+        self._type = type
+        self._hp = hp
+        self._defense = defense
+        self._atk_name = atk_name
+        self._atk_dmg = atk_dmg
+
+
+
